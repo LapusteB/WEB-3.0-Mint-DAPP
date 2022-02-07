7d8de8f1f2db9c819e5556d87515205527f9632e
Brian Lapuste <seblapuste@gmail.com>
2022-02-07 15:40:27 -0500

    update

--- src/App.js
+++ src/App.js
@@ -163,8 +163,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 4) {
-      newMintAmount = 4;
+    if (newMintAmount > 2) {
+      newMintAmount = 2;
     }
     setMintAmount(newMintAmount);
   };
@@ -204,13 +204,13 @@ function App() {
       >
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         <s.SpacerSmall />
-        <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
+        <ResponsiveWrapper flex={1} style={{ padding: 128 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg alt={"example"} src={"/config/images/example.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
-            flex={2}
+            flex={0.5}
             jc={"center"}
             ai={"center"}
             style={{
